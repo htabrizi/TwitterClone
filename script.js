46261cdbbb34f81c8592f9f2b7b93db36bdcc569
Hamid-Tabrizi <h.tabrizi@orbs.ir>
2020-07-16 08:48:48 +0430

    93 Javascript login page validation

--- script.js
+++ script.js
@@ -1,38 +1,53 @@
 //DOM Elements
 const mainPage = document.querySelector('.main-page');
 const loginpage = document.querySelector('.login-page');
-const middleContent=document.querySelector('.middle-content');
-const btnTop=document.querySelector('.btn-top');
-const newsFeedPage =document.querySelector('.feeds-page');
-const loginModal=document.querySelector('.login-modal');
+const middleContent = document.querySelector('.middle-content');
+const btnTop = document.querySelector('.btn-top');
+const newsFeedPage = document.querySelector('.feeds-page');
+const loginModal = document.querySelector('.login-modal');
+const modalX = document.querySelector('.login-modal i');
+const loginFormBtn = document.querySelector('.login-form-btn')
 /*************************************************************/
 /*************************************************************/
-
 //main page
-const goToLoginPage=()=> {
+const goToLoginPage = () => {
     mainPage.style.display = 'none';
     loginpage.style.display = 'grid';
 };
-middleContent.addEventListener('click',e =>
-{
-   if (e.target.classList[1] === 'main-btn')
-   {
-       goToLoginPage();
-   }
+middleContent.addEventListener('click', e => {
+    if (e.target.classList[1] === 'main-btn') {
+        goToLoginPage();
+    }
 
 });
-btnTop.addEventListener('click',()=>{
-    const inputUserInfo=document.querySelector('.user-info');
-    const inputPassword=document.querySelector('.password');
+btnTop.addEventListener('click', () => {
+    const inputUserInfo = document.querySelector('.user-info');
+    const inputPassword = document.querySelector('.password');
 
-    if(inputUserInfo.value!=="" && inputPassword.value!==""){
-mainPage.style.display='none';
-newsFeedPage.style.display='block';
+    if (inputUserInfo.value !== "" && inputPassword.value !== "") {
+        mainPage.style.display = 'none';
+        newsFeedPage.style.display = 'block';
 
-    }
-    else{
+    } else {
         goToLoginPage()
-        loginModal.style.display='block';
+        loginModal.style.display = 'block';
     }
 });
 
+//login page
+modalX.addEventListener('click', () => {
+    loginModal.style.display = 'none';
+});
+
+loginFormBtn.addEventListener('click', () => {
+    const loginUserInfo = document.querySelector('.login-user-info');
+    const loginPassword = document.querySelector('.loginPassword');
+
+if(loginUserInfo.value!=="" && loginPassword.value!==""){
+    loginpage.style.display='none';
+    newsFeedPage.style.display='block';
+}
+else{
+    loginModal.style.display='block'
+}
+});
