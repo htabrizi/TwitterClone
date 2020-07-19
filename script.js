433d03ebbde2db29d2a92171686656503a792d8a
Hamid-Tabrizi <h.tabrizi@orbs.ir>
2020-07-19 19:59:06 +0430

    97 javascript make post modal work ERROR to load page

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
 //DOM Elements
 const mainPage = document.querySelector('.main-page');
-const loginpage = document.querySelector('.login-page');
+const loginpage = document.quer
+ySelector('.login-page');
 const middleContent = document.querySelector('.middle-content');
 const btnTop = document.querySelector('.btn-top');
 const newsFeedPage = document.querySelector('.feeds-page');
 const loginModal = document.querySelector('.login-modal');
 const modalX = document.querySelector('.login-modal i');
-const loginFormBtn = document.querySelector('.login-form-btn')
+const loginFormBtn = document.querySelector('.login-form-btn');
+const postBtn = document.querySelector('.post-btn');
+const modalWrapper= document.querySelector('.modal-wrapper');
+const modal=document.querySelector('.modal');
+const postModalX=document.querySelector('.modal-header i'); 
+const modalPostBtn=document.querySelector('.modal-header button');
+const modalFooterPlus=document.querySelector('.modal-footer span');
+const modalInput=document.querySelector('.modal-input');
+
 /*************************************************************/
 /*************************************************************/
-//main page
+//main page  
 const goToLoginPage = () => {
     mainPage.style.display = 'none';
     loginpage.style.display = 'grid';
@@ -50,4 +59,39 @@ if(loginUserInfo.value!=="" && loginPassword.value!==""){
 else{
     loginModal.style.display='block'
 }
+});
+//News feed page
+//Post modal
+postBtn.addEventListener('click',() => {
+    modal.style.display='block';
+   console.log( modalWrapper.classList.add('modal-wrapper-display'));
+
+});
+
+const changeOpacity = x =>{
+modalPostBtn.style.opacity=x;
+modalFooterPlus.style.opacity=x;
+};
+
+postModalX.addEventListener('click',()=>{
+    modal.style.display='none';
+    modalWrapper.classList.remove('modal-wrapper-display');
+    if(modalInput.value !==""){
+        modalInput.value=""; 
+        changeOpacity(.5);
+    }
+});
+modalInput.addEventListener
+('keypress', e =>{
+    if(e.target.value !==""){
+        changeOpacity(1);
+
+    }
+});
+modalInput.addEventListener('blur',(e)=>{
+    if(e.target.value===''){{
+        changeOpacity(.5);
+    }
+
+    }
 });
