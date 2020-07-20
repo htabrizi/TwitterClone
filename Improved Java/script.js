bfbaa53167141e465a67c380a07cd13181302953
Hamid-Tabrizi <h.tabrizi@orbs.ir>
2020-07-20 11:30:23 +0430

    4-3 Improved JS

---
+++ Improved Java/script.js
@@ -0,0 +1,41 @@
+// Lecture: Get And Manipulate On Individual Elements - Part 1
+
+/*
+console.log(document.getElementById('list'));
+
+var el = document.getElementById('list');
+console.log(el);
+console.log(typeof el);
+
+// var el = document.getElementById('LIST');
+// console.log(el);
+
+console.log(el.textContent);
+console.log(el.textContent = 'Hello');
+
+var heading = document.getElementById('heading').textContent;
+console.log(heading);
+
+var ulElement = document.getElementById('list');
+console.log(ulElement.textContent = heading);
+
+console.log(el.innerHTML);
+console.log(el.innerHTML = 'Hello');
+console.log(el.innerHTML = '<h1>Hello</h1>');
+console.log(el.textContent = '<h1>Hello</h1>');
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
