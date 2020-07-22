6d1b8b8b476beec1659e1df5abe3b3f9e772272c
Hamid-Tabrizi <h.tabrizi@orbs.ir>
2020-07-22 23:24:55 +0430

    4-16. Creating Elements

--- Improved Java/script.js
+++ Improved Java/script.js
@@ -93,8 +93,13 @@ console.log(el.textContent = '<h1>Hello</h1>');
 //     input.setAttribute('value', input.value);
 // })
 
-var listItem=document.getElementById("list-item");
+var newEl = document.createElement('button');
+var text = document.createTextNode('click');
+var form = document.getElementById('add');
+newEl.setAttribute('style','display:block;background:green')
+newEl.appendChild(text);
 
-
-
-  
+console.log(newEl);
+ form.appendChild(newEl);
+form.insertBefore(newEl,form.children[1]);
+newEl.parentElement.removeChild(newEl);
