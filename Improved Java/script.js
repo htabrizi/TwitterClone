000ddb997e2405bcc996ffd8ce21aef1d0dd2a27
Hamid-Tabrizi <67880123+htabrizi@users.noreply.github.com>
2020-07-24 18:59:04 +0430

    5-3 work in Objects

--- Improved Java/script.js
+++ Improved Java/script.js
@@ -1,12 +1,21 @@
- function a()
- {
-     console.log('hello');
+var person1={};
+person1.firstname='john';
+person1.lastname='smith';
 
- }
- a['prop']='hi';
- a.obj={
-     greet:'hey'
- };
- a.myFunc=function(){
-     console.log('hola');
- }
+var person2={};
+person2.firstname='nik';
+person2.lastname='dou';
+
+function createPerson(first,last){
+    
+  this.first=first;
+    this.last=last;
+   
+}
+var p3=new createPerson('1','t');
+console.log(p3);
+
+
+var strObj=new String('hello');
+console.log(typeof strObj);
+console.log(strObj);
