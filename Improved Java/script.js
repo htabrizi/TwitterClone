04a7bfcf439e611489345a62705b6a91baf331b6
Hamid-Tabrizi <67880123+htabrizi@users.noreply.github.com>
2020-07-24 19:20:20 +0430

    5-4 work in Objects

--- Improved Java/script.js
+++ Improved Java/script.js
@@ -1,21 +1,2 @@
-var person1={};
-person1.firstname='john';
-person1.lastname='smith';
-
-var person2={};
-person2.firstname='nik';
-person2.lastname='dou';
-
-function createPerson(first,last){
-    
-  this.first=first;
-    this.last=last;
-   
-}
-var p3=new createPerson('1','t');
-console.log(p3);
-
-
-var strObj=new String('hello');
-console.log(typeof strObj);
-console.log(strObj);
+var a = {}
+var b=new Object();
