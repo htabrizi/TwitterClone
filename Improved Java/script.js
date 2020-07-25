db5bcf7234e8528751d71c498bc938b290491309
Hamid-Tabrizi <67880123+htabrizi@users.noreply.github.com>
2020-07-25 22:02:45 +0430

    5-8 work in Objects proto

--- Improved Java/script.js
+++ Improved Java/script.js
@@ -1,13 +1,14 @@
-var scores =[55,35,87,45];
-function checkResult(arr,fn){
-    var newArr=[];
-    for(var i=0; i<arr.length; i++){
-    newArr.push(fn(arr[i]));
-    }
-    return newArr;
+function finalResult (score){
+    if (score>81){
+        return function (name){
+            console.log(name + ',you Passed');
+        }
+    else if(score >=51){
+return function (name){
+    console.log(name +',passed');
+}
+}
 
 }
-function passOrFail(score){
-    return score >= 51;
-}
-console.log(checkResult(scores,passOrFail));
+var result1 = finalResult(77);
+console.log(result1); 
