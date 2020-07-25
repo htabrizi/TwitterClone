549a30423c6e789d72613993063b9c849c653fbd
Hamid-Tabrizi <67880123+htabrizi@users.noreply.github.com>
2020-07-25 21:41:09 +0430

    5-7 work in Objects proto

--- Improved Java/script.js
+++ Improved Java/script.js
@@ -1,11 +1,13 @@
-var fristProto={
-    sayHello:function(){
-        return  this.name;
+var scores =[55,35,87,45];
+function checkResult(arr,fn){
+    var newArr=[];
+    for(var i=0; i<arr.length; i++){
+    newArr.push(fn(arr[i]));
     }
-};
-var john =Object.create(firstProto,{
-    name:{
-        value:'john'
-    }
-});
-console.log(john);
+    return newArr;
+
+}
+function passOrFail(score){
+    return score >= 51;
+}
+console.log(checkResult(scores,passOrFail));
