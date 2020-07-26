189ce0695f0231e8aa2e0923c1bfb2c9286e6ff2
Hamid-Tabrizi <h.tabrizi@orbs.ir>
2020-07-26 20:16:49 +0430

    6-1 Json

--- Improved Java/script.js
+++ Improved Java/script.js
@@ -1,32 +1,6 @@
-function a(){
-    var arr =[];
-    for (var i=0; i<3;i++){
-        arr.push(()=>{console.log(i);}
-
-        )};
-return arr;
-
-}
-
-var person={
-    firstname:'john',
-    lastname:'smith',
-    fullname: function(){
-        return this.firstname+' '+this.lastname;
-    }
+var myObj={
+    name:'john',
+    age:27
 };
-var nick={
-    firstname:'Nick',
-    lastname:'Doe'
-      
-};
-console.log(person.fullname.call(nick));
-var definePerson= function(age,job){
-    console.log(this.fullname()+' is ' + age+' years old and he is a '+ job);
-
-};
-definePerson.call(person,28,'developer');
-definePerson.apply(person,[28,"postman"]);
-var getPerson= definePerson.bind(person,25);
-getPerson('musician');
-getPerson('28')
+var myData=JSON.stringify(myObj);
+    console.log(myData);
