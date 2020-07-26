b753786deab542f188f88834992cc2cf33300aae
Hamid-Tabrizi <h.tabrizi@orbs.ir>
2020-07-26 20:34:25 +0430

    6-2 JSON & AJAX

--- Improved Java/script.js
+++ Improved Java/script.js
@@ -1,6 +1,9 @@
-var myObj={
-    name:'john',
-    age:27
-};
-var myData=JSON.stringify(myObj);
+var xmlhttp = new XMLHttpRequest();
+xmlhttp.open("Get", "/.users.json", true);
+xmlhttp.onload = function () {
+  if (this.status == 200) {
+    var myData = JSON.parse(this.responseText);
     console.log(myData);
+  }
+} 
+xmlhttp.send();
