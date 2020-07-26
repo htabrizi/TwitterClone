0c34eddf05057eea6e7ad82d9f917368bbf5e8ca
Hamid-Tabrizi <h.tabrizi@orbs.ir>
2020-07-26 16:50:36 +0430

    5-11 call,apply,bind

--- Improved Java/script.js
+++ Improved Java/script.js
@@ -8,3 +8,25 @@ return arr;
 
 }
 
+var person={
+    firstname:'john',
+    lastname:'smith',
+    fullname: function(){
+        return this.firstname+' '+this.lastname;
+    }
+};
+var nick={
+    firstname:'Nick',
+    lastname:'Doe'
+      
+};
+console.log(person.fullname.call(nick));
+var definePerson= function(age,job){
+    console.log(this.fullname()+' is ' + age+' years old and he is a '+ job);
+
+};
+definePerson.call(person,28,'developer');
+definePerson.apply(person,[28,"postman"]);
+var getPerson= definePerson.bind(person,25);
+getPerson('musician');
+getPerson('28')
