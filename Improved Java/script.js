dbab25a8b4128cf9e3828a8d7442d02cd9b593bc
Hamid-Tabrizi <67880123+htabrizi@users.noreply.github.com>
2020-07-27 13:45:32 +0430

    test

--- Improved Java/script.js
+++ Improved Java/script.js
@@ -1,10 +1,11 @@
-function a(){
-    var arr =[];
-    for (var i=0; i<3;i++){
-        arr.push(()=>{console.log(i);}
-
-        )};
-return arr;
-
-}
-
+var person={
+    firstname:'john',
+    Lastname:'smith',
+    fullname: function(){
+        return this.firstname+this.Lastname;
+    }
+};
+var definePerson= function(age,job){
+    console.log(this.fullname()+age+job);
+};
+ definePerson(person,28,'develope')
