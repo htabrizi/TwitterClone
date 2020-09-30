f464f194d3cbc7a6afe23f9bea1077cf07ed76de
Hamid-Tabrizi <h.tabrizi@orbs.ir>
2020-09-30 18:10:43 +0330

    Work with Darkmode give class css js

--- script.js
+++ script.js
@@ -119,10 +119,13 @@ xBtn.addEventListener('click' ,()=>{
 //darkmode
 const darkElements1 = document.querySelectorAll('.dark-mode-1')
 const darkElements2 = document.querySelectorAll('.dark-mode-2')
+const lighTexts=document.querySelectorAll('.light-text')
+const borders=document.querySelectorAll('.border')
 
 toggle.addEventListener('click',()=>{
    circle.classList.toggle('move');
    Array.from(darkElements1).map(darkEl1=>darkEl1.classList.toggle('dark-1'));
    Array.from(darkElements2).map(darkEl2=>darkEl2.classList.toggle('dark-2'));
-
+   Array.from(lighTexts).map(lighText=>lighText.classList.toggle('light'));
+   Array.from(borders).map(border=>border.classList.toggle('border-color'))
 });
